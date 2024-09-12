5b6d2a150ff80e4ecb6cdae75b868a272d8a1593
faizbyp <faizbyp@gmail.com>
2024-09-12 09:31:22 +0700

    feat: get company by type and group

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,9 +1,11 @@
-const { getCompanyById } = require("../models/CompanyModel");
+const { getCompanyById, getAllCompany } = require("../models/CompanyModel");
 const { getAllFromTable } = require("../models/StandardQuery");
 
 const handleGetAllCompany = async (req, res) => {
+  const type = req.query.type || null;
+  const group = req.query.group || null;
   try {
-    let result = await getAllFromTable("mst_company");
+    let result = await getAllCompany(type, group);
     res.status(200).send({
       message: `Success get companies`,
       data: result,
--- models/CompanyModel.js
+++ models/CompanyModel.js
@@ -1,6 +1,27 @@
 const db = require("../config/connection");
 const TRANS = require("../config/transaction");
 
+const getAllCompany = async (type, group) => {
+  const client = await db.connect();
+  try {
+    await client.query(TRANS.BEGIN);
+    const query = `
+    SELECT * FROM mst_company c 
+    WHERE (c.company_type = $1 OR $2::VARCHAR IS NULL) 
+    AND (c.company_group = $3 OR $4::VARCHAR IS NULL);
+    `;
+    const result = await client.query(query, [type, type, group, group]);
+    await client.query(TRANS.COMMIT);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    await client.query(TRANS.ROLLBACK);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
 const getCompanyById = async (id) => {
   const client = await db.connect();
   try {
@@ -21,5 +42,6 @@ const getCompanyById = async (id) => {
 };
 
 module.exports = {
+  getAllCompany,
   getCompanyById,
 };
