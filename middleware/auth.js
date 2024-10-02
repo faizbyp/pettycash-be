c094276cc36d17c3fc9e3adc8edb3367b617a346
faizbyp <faizbyp@gmail.com>
2024-10-02 14:25:11 +0700

    fix: is auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,46 @@
 const jwt = require("jsonwebtoken");
 
 const isAuth = (req, res, next) => {
-  // Get the token from the Authorization header
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
-
-  if (token == null) {
-    // If no token is provided, block the request
-    return res.status(401).send({ message: "Unauthorized: No token provided" });
-  }
-
-  // Verify the token using the secret key
-  jwt.verify(token, process.env.SECRETJWT, (err, user) => {
-    if (err) {
-      // If the token is invalid or expired, block the request
-      return res.status(401).send({ message: "Forbidden: Invalid or expired token" });
+  let headers = req.headers.Authorization || req.headers.authorization;
+  let token = headers?.split(" ")[1];
+  let decode;
+  if (!(req.headers.authorization || req.headers.Authorization)) {
+    res.status(401).send({
+      message: "Access Denied",
+    });
+  } else {
+    try {
+      if (token !== undefined) {
+        decode = jwt.verify(token, process.env.SECRETJWT);
+      } else {
+        const exception = new Error();
+        exception.name = "Unauthorized";
+        exception.response = {
+          status: 401,
+          data: {
+            message: "Unauthorized",
+          },
+        };
+        throw exception;
+      }
+      req.useridSess = decode.id;
+      next();
+    } catch (err) {
+      if (err?.response?.status === 401) {
+        res.status(401).send({
+          message: err.response.data.message,
+        });
+      } else if (err.name == "TokenExpiredError") {
+        res.status(403).send({
+          message: err.message,
+        });
+      } else {
+        res.status(500).send({
+          message: err.stack,
+        });
+      }
     }
-
-    // If token is valid, store user information (from the payload) in the request
-    req.user = user;
-
-    // Proceed to the next middleware or route handler
-    next();
-  });
+  }
 };
 
 module.exports = isAuth;
